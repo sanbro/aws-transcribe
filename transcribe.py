25f85eaaf9d1586a1703bb070f701614141ceec7
sanbro <sanbro18@gmail.com>
2024-12-05 17:24:04 +0400

    filename as object key name

--- transcribe.py
+++ transcribe.py
@@ -13,7 +13,6 @@ output_bucket = "transcibe-output-source"
 
 
 def lambda_handler(event, context):
-    # print("Received event: " + json.dumps(event, indent=2))
 
     # Get the object from the event and show its content type
     bucket = event["Records"][0]["s3"]["bucket"]["name"]
@@ -29,14 +28,17 @@ def lambda_handler(event, context):
             print(f"Invalid file type for transcription: {file_key}")
             return
         job_name = f"transcribe-job-{int(time.time())}"
-
+        file_name = os.path.basename(file_key)
+        file_name_without_extension, _ = os.path.splitext(file_name)
+        print(f"Base name : {file_name_without_extension}")
+        output_key = f"subtitle/{file_name_without_extension}"
         # Create the transcription job
         transcribe_job_uri = f"s3://{bucket}/{file_key}"
         response = transcribe.start_transcription_job(
             TranscriptionJobName=job_name,
             Media={"MediaFileUri": transcribe_job_uri},
             OutputBucketName=output_bucket,
-            OutputKey="my-output-files/{file_key}/",
+            OutputKey=output_key,
             LanguageCode="en-US",
             Subtitles={"Formats": ["vtt", "srt"], "OutputStartIndex": 1},
         )
